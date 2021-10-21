1163d25e3cf89ece9f205debdeda32ae65c47b6c
qkrtndh <qkrtndh5@naver.com>
2021-10-21 16:37:25 +0900

    test setting

--- vue-tazan/src/App.vue
+++ vue-tazan/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TourList/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TourList from './components/list/TourList.vue'
 export default {
+  
   name: 'App',
   components: {
-    HelloWorld
+    TourList
   }
 }
 </script>
