5bb2c79e2248f351eaa5929ca2fb2a1ca01c36b8
xerrapin <dlawogur14@naver.com>
2021-10-25 14:22:11 +0900

    vue2-update

--- vue2-front/src/App.vue
+++ vue2-front/src/App.vue
@@ -1,60 +1,45 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+  <div id="app">
+    <Header/>
+    <Main/>
+    <TourList/>
+    <Footer/>
+  </div>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld';
+
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+
+import TourList from './components/list/TourList.vue'
+
+//import Main from './components/main/main.vue'
+
+
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
-  },
+    // Main,
+    Header,
+    Footer,
+    TourList
 
-  data: () => ({
-    //
-  }),
-};
+  }
+}
 </script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+
+}
+
+</style>
