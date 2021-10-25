982e6c9b4ab1d97792796c3ab7eda9bac1e57e66
xerrapin <dlawogur14@naver.com>
2021-10-25 13:26:22 +0900

    Footer, Header modified

--- vue-tazan/src/components/main/main.vue
+++ vue-tazan/src/components/main/main.vue
@@ -1,32 +1,5 @@
 <template>
   <div>
-    <!-- 맨위 nav바 내용 -->
-    <!-- Start Top Nav 
-    <nav class="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top">
-      <div class="container text-light">
-        <div class="w-100 d-flex justify-content-between">
-          <div>
-            <i class="fa fa-envelope mx-2"></i>
-            <a class="navbar-sm-brand text-light text-decoration-none" href="#">info@company.com</a>
-            <i class="fa fa-phone mx-2"></i>
-            <a class="navbar-sm-brand text-light text-decoration-none" href="#">010-000-0000</a>
-          </div>
-          <div>
-            <a class="text-light" href="#" target="_blank" rel="sponsored"><i
-                class="fab fa-facebook-f fa-sm fa-fw me-2"></i></a>
-            <a class="text-light" href="#" target="_blank"><i class="fab fa-instagram fa-sm fa-fw me-2"></i></a>
-            <a class="text-light" href="#" target="_blank"><i class="fab fa-twitter fa-sm fa-fw me-2"></i></a>
-            <a class="text-light" href="#" target="_blank"><i class="fab fa-linkedin fa-sm fa-fw"></i></a>
-          </div> 
-        </div>
-      </div>
-    </nav>
-    Close Top Nav -->
-
-    <!-- Header -->
-
-    <!-- Close Header -->
-
     <!-- Modal -->
     <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog"
       aria-labelledby="exampleModalLabel" aria-hidden="true">
