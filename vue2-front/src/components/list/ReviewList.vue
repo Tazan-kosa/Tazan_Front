460fd69269079e96daee11e855b9fca1aa228fa7
qkrtndh <qkrtndh5@naver.com>
2021-10-26 16:48:29 +0900

    ReviewList datepicker & axios

--- vue2-front/src/components/list/ReviewList.vue
+++ vue2-front/src/components/list/ReviewList.vue
@@ -12,13 +12,13 @@
         </div>
       </div>
     </header>
-    <!--main page-->
+
     <section class="py-0">
       <div class="container px-4 px-lg-5 mt-3">
-        <!--        <CheckBox :tagname="Tagnames" :checkedtag="checkedtag" @checkedtag="checkedtaglist" />-->
+        <Select @selectedDate="selectedDate" :datevalue="startdate"/>
+        <p class="datewarning" v-if="startdate>enddate"><b>날짜입력 오류입니다. 다시 확인해주세요.</b></p>
         <CheckBox :tagname="Tagnames" :checkedtag="checkedtag"/>
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-          <!-- <Select/> -->
           <Box v-for="(item, i) in TourItemList" :key="i" :item="item" :checkedtag="checkedtag"/>
         </div>
       </div>
@@ -27,34 +27,32 @@
 </template>
 <script>
 
-// import TourItemList from './sample.js'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import CheckBox from "@/components/list/CheckBox";
 import Tagname from "@/components/list/tagnames";
 import Box from './Box.vue'
 import axios from 'axios'
+import Select from "@/components/list/Select";
 
 export default {
-  name: 'TourList',
+  name: 'ReviewList',
   data() {
     return {
-      // TourItemList: TourItemList,
       TourItemList: [],
       Tagnames: Tagname,
-      checkedtag: ['관광지', '문화시설', '축제공연행사', '여행코스', '레포츠', '숙박', '쇼핑', '음식점']
+      checkedtag: ['관광지', '문화시설', '축제공연행사', '여행코스', '레포츠', '숙박', '쇼핑', '음식점'],
+      startdate:'',
+      enddate:'',
     }
   },
   components: {
     Box,
     CheckBox,
+    Select
 
   },
   methods: {
-    // checkedtaglist(tags) {
-    //   this.checkedtag = tags
-    //   console.log(this.checkedtag)
-    // }
     searchTourList() {
       const value = document.getElementById("searchbar").value
       axios.get(`http://210.178.22.18:3000/search/${value}`)
@@ -65,9 +63,35 @@ export default {
           .catch(function (err) {
             console.log("에러발생: " + err)
           })
+    },
+    selectedDate({start,end}){
+      this.startdate=start
+      this.enddate=end
+      if(start<=end){
+        axios.get(`http://210.178.22.18:3000/selectdate/${this.startdate}/${this.enddate}`)
+            .then(result => {
+              console.log(result.data)
+            })
+            .catch(function (err) {
+              console.log("에러발생: " + err)
+            })
+      }
     }
   },
   created() {
+    var temp =new Date()
+    var year = temp.getFullYear();
+    var month = temp.getMonth() + 1;
+    var day = temp.getDate();
+
+    if(month < 10){
+      month = '0' + month;
+    }
+    if(day < 10){
+      day = '0' + day;
+    }
+    this.startdate=year + '-' + month + '-' + day
+    this.enddate=year + '-' + month + '-' + day
     axios.get('http://210.178.22.18:3000/test')
         .then(result => {
           this.TourItemList = result.data.sample
@@ -91,6 +115,13 @@ export default {
 }
 
 header {
-  background: #008E9B;
+  background: #00BF72;
+}
+
+.datewarning{
+  color: red;
+}
+.datewarning b{
+  font-size: 15px;
 }
 </style>
