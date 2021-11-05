de97c3d97c871a6e6c8b59c64710fcae28608e19
qkrtndh <qkrtndh5@naver.com>
2021-11-05 16:12:07 +0900

    reviewList

---
+++ vue2-front/src/components/list/ReviewBox.vue
@@ -0,0 +1,54 @@
+<template>
+  <!--  <div class="itembox mb-2" v-if="checkedtag.includes(item.tag1) || checkedtag.includes(item.tag2)" @click="viewURL(item.url)">-->
+  <div class="itembox mb-2" @click="viewURL(item.url)">
+    <ReviewItem :item="item"/>
+  </div>
+</template>
+
+<script>
+import ReviewItem from './ReviewItem'
+
+export default {
+  name: 'ReviewBox',
+  components: {
+    ReviewItem,
+  },
+  props: {
+    item: Object,
+    checkedtag: Array
+  },
+  methods: {
+    viewURL(link) {
+      window.open(link)
+    }
+  }
+}
+</script>
+
+<style scoped>
+div.itembox {
+  width: 265px;
+  margin: 5px;
+  padding: 0;
+}
+
+div.itembox {
+  transform: scale(1);
+  -webkit-transform: scale(1);
+  -moz-transform: scale(1);
+  -ms-transform: scale(1);
+  -o-transform: scale(1);
+  transition: all 0.3s ease-in-out; /* 부드러운 모션을 위해 추가*/
+}
+
+div.itembox:hover {
+  cursor: pointer;
+  background: #845EC2;
+  border-radius: 10px;
+  transform: scale(1.1);
+  -webkit-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -ms-transform: scale(1.1);
+  -o-transform: scale(1.1);
+}
+</style>
--- vue2-front/src/components/list/ReviewList.vue
+++ vue2-front/src/components/list/ReviewList.vue
@@ -17,11 +17,11 @@
 
     <section class="py-0">
       <div class="container px-4 px-lg-5 mt-3">
-        <Select @selectedDate="selectedDate" :datevalue="startdate"/>
+        <Select @selectedDate="selectedDate" :enddate="enddate" :startdate="startdate"/>
         <p class="datewarning" v-if="startdate>enddate"><b>날짜입력 오류입니다. 다시 확인해주세요.</b></p>
         <CheckBox :tagname="Tagnames" :checkedtag="checkedtag"/>
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-          <Box v-for="(item, i) in TourItemList" :key="i" :item="item" :checkedtag="checkedtag"/>
+          <ReviewBox v-for="(item, i) in ReviewItemList" :key="i" :item="item" :checkedtag="checkedtag"/>
         </div>
       </div>
     </section>
@@ -33,7 +33,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import CheckBox from "@/components/list/CheckBox";
 import Tagname from "@/components/list/tagnames";
-import Box from './Box.vue'
+import ReviewBox from './ReviewBox'
 import axios from 'axios'
 import Select from "@/components/list/Select";
 
@@ -41,16 +41,15 @@ export default {
   name: 'ReviewList',
   data() {
     return {
-      TourItemList: [],
+      ReviewItemList: [],
       Tagnames: Tagname,
       checkedtag: ['관광지', '문화시설', '축제공연행사', '여행코스', '레포츠', '숙박', '쇼핑', '음식점'],
       startdate: '',
       enddate: '',
-      TourListCnt: 0
     }
   },
   components: {
-    Box,
+    ReviewBox,
     CheckBox,
     Select
 
@@ -60,8 +59,7 @@ export default {
       const value = document.getElementById("searchbar").value
       axios.get(`http://kosa3.iptime.org:50201/search/${value}`)
           .then(result => {
-            console.log(result.data)
-
+            this.ReviewItemList=result.data
           })
           .catch(function (err) {
             console.log("에러발생: " + err)
@@ -95,9 +93,9 @@ export default {
     }
     this.startdate = year + '-' + month + '-' + day
     this.enddate = year + '-' + month + '-' + day
-    axios.get('http://kosa3.iptime.org:50201/testdb')
+    axios.get('http://kosa3.iptime.org:50201/review/reviewList')
         .then(result => {
-          this.TourItemList = result.data
+          this.ReviewItemList=result.data
         })
         .catch(function (err) {
           console.log("에러발생: " + err)
