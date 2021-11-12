9211cb377ff7560e2d2bf57fdeb598376a6c666e
Yunhyunjo <gotqlc147@likelion.org>
2021-11-12 10:10:28 +0900

    review merge

--- vue2-front/src/components/list/ReviewList.vue
+++ vue2-front/src/components/list/ReviewList.vue
@@ -57,7 +57,7 @@ export default {
   methods: {
     searchTourList() {
       const value = document.getElementById("searchbar").value
-      axios.get(`http://localhost:80/review/search/${value}`)
+      axios.get(`http://kosa3.iptime.org:50201/review/search/${value}`)
           .then(result => {
             this.ReviewItemList=result.data
             console.log(this.ReviewItemList)
@@ -70,7 +70,7 @@ export default {
       this.startdate = start
       this.enddate = end
       if (start <= end) {
-        axios.get(`http://localhost:80/review/selectdate/${this.startdate}/${this.enddate}`)
+        axios.get(`http://kosa3.iptime.org:50201/review/selectdate/${this.startdate}/${this.enddate}`)
             .then(result => {
               console.log(result.data)
               this.ReviewItemList = result.data
@@ -95,7 +95,7 @@ export default {
     }
     this.startdate = year + '-' + month + '-' + day
     this.enddate = year + '-' + month + '-' + day
-    axios.get('http://localhost:80/review/reviewList')
+    axios.get('http://kosa3.iptime.org:50201/review/reviewList')
         .then(result => {
           this.ReviewItemList=result.data
         })
