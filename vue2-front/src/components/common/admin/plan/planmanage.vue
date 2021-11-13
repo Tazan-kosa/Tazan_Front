6a9653c79d6d01c85d8ebc06a0ee8c6ac05258f9
tndh qkr <qkrtndh5@naver.com>
2021-11-13 19:59:04 +0900

    admin page 50%

---
+++ vue2-front/src/components/common/admin/plan/planmanage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <planmanageItem v-for="(item, i) in tourList" :key="i" :item="item"/>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import planmanageItem from "./planmanageItem";
+
+export default {
+  name: "planmanage",
+  data() {
+    return {
+      tourList: ''
+    }
+  },
+  components: {
+    planmanageItem
+  },
+  created() {
+    axios.get('http://kosa3.iptime.org:50201/tourList')
+        .then(result => {
+          this.tourList = result.data
+        })
+        .catch(function (err) {
+          console.log("에러발생: " + err)
+        })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue2-front/src/components/common/admin/review/reviewmanageItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{this.item.reviewTitle}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "reviewmanageItem",
+  props:{
+    item:Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
