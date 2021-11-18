86a75076410c45efdf050c522069caa0145d2398
devkimmjen <wpals814@gmail.com>
2021-11-18 12:31:29 +0900

    디테일 부분 수정 사항

--- vue2-front/src/components/detail/CreatePage/Unkown.vue
+++ vue2-front/src/components/detail/CreatePage/Unkown.vue
@@ -19,7 +19,7 @@
                     v-model="text"
                     size="sm"
                     class="w-25 p-3 mb-1 text-black"
-                    placeholder="여행 제목을 적어 주세요."
+                    placeholder="여행 제목을 적어 주세요.(공백포함 1자이상 45자이하 입니다!)"
                 ></b-form-input>
               </div>
             </div>
